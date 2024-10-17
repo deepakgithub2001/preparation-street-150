db7c41c241a623a6fae34dfd061e06cf40c41cf8
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-17 15:48:22 +0530

    add even odd file.

---
+++ first_50_basics/01_isEvenOdd.js
@@ -0,0 +1,10 @@
+function isEvenOdd(number) {
+    if (number % 2 == 0) {
+        return ("Even number");
+    }
+    else {
+        return ("Odd number");
+    }
+}
+console.log(isEvenOdd(7));
+console.log(isEvenOdd(10));
